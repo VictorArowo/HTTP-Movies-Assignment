3ec3816f25928d17473d92427e4810f938b32d2f
Victor Arowo <arowovici@gmail.com>
2019-10-17 12:59:01 +0100

    Added Functionality for DEditing Movies

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,58 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Route } from 'react-router-dom';
 import SavedList from './Movies/SavedList';
 import MovieList from './Movies/MovieList';
 import Movie from './Movies/Movie';
 import UpdateMovie from './Movies/UpdateMovie';
+import axios from 'axios';
 
 const App = () => {
+  const [movieList, setMovieList] = useState(null);
   const [savedList, setSavedList] = useState([]);
 
   const addToSavedList = movie => {
     setSavedList([...savedList, movie]);
   };
 
+  useEffect(() => {
+    axios
+      .get('http://localhost:5000/api/movies')
+      .then(res => setMovieList(res.data))
+      .catch(err => console.log(err.response));
+  }, []);
   return (
     <>
       <SavedList list={savedList} />
-      <Route exact path="/" component={MovieList} />
+      <Route
+        exact
+        path="/"
+        component={MovieList}
+        setMovieList={setMovieList}
+        movieList={movieList}
+      />
       <Route
         path="/movies/:id"
         render={props => {
-          return <Movie {...props} addToSavedList={addToSavedList} />;
+          return (
+            <Movie
+              {...props}
+              addToSavedList={addToSavedList}
+              movieList={movieList}
+              setMovieList={setMovieList}
+            />
+          );
         }}
       />
       <Route
         path="/update-movie/:id"
         render={props => {
-          return <UpdateMovie {...props} />;
+          return (
+            <UpdateMovie
+              {...props}
+              movieList={movieList}
+              setMovieList={setMovieList}
+            />
+          );
         }}
       />
     </>
--- client/src/Movies/UpdateMovie.js
+++ client/src/Movies/UpdateMovie.js
@@ -1,7 +1,64 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import axios from 'axios';
+const UpdateMovie = ({
+  match: {
+    params: { id }
+  },
+  movieList,
+  history
+}) => {
+  const movieToEdit = movieList.find(item => item.id === Number(id));
 
-const UpdateMovie = () => {
-  return <div></div>;
+  const updateMovie = ({ id, title, director, metascore, stars }) => {
+    axios
+      .put(`http://localhost:5000/api/movies/${id}`, {
+        id,
+        title,
+        director,
+        metascore,
+        stars
+      })
+      .then(() => {
+        history.push('/');
+      })
+      .catch(err => {});
+  };
+
+  return (
+    <Formik
+      //   key={currentQuoteId}
+      //   validate={validate}
+      initialValues={{
+        title: movieToEdit.title,
+        director: movieToEdit.director,
+        metascore: movieToEdit.metascore
+      }}
+      onSubmit={({ title, director, metascore }) => {
+        updateMovie({
+          id: Number(id),
+          title,
+          director,
+          metascore,
+          stars: movieToEdit.stars
+        });
+      }}
+      render={() => (
+        <Form>
+          <Field name="title" />
+          <ErrorMessage name="title" component="span" />
+
+          <Field name="director" />
+          <ErrorMessage name="director" component="span" />
+
+          <Field name="metascore" />
+          <ErrorMessage name="metascore" component="span" />
+
+          <input type="submit" />
+        </Form>
+      )}
+    />
+  );
 };
 
 export default UpdateMovie;
